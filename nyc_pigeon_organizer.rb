ee98dd87a8c0d846fc150948518f5bdd28fc4753
George Cooper <coopsgeorge@gmail.com>
2020-03-08 10:48:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -30,7 +30,7 @@ end
 
 keys = organized_hash.keys
 
-data[:color].each do |bird_color, name|
+data[:color].each do |pigeon_color, name|
  name.each do |bird_name|
    keys.each do |item|
      if bird_name === item
