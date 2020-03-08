7400e500a6d1ebe82cbab4c97c2833d5765bddb6
George Cooper <coopsgeorge@gmail.com>
2020-03-08 10:44:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,12 +18,12 @@ pigeon_data = {
 }
 
 def nyc_pigeon_organizer(data)
-final = {}
+organized_hash = {}
 
 data.each do |first_level, all_other|
   all_other.each do |category, array|
     array.each do |name|
-     final[name] = {:color => [], :gender => [], :lives => []}
+     organized_hash[name] = {:color => [], :gender => [], :lives => []}
    end
   end
 end
