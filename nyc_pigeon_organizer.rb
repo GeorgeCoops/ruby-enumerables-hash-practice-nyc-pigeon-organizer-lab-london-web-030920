edec9f3a4dc80902d9a845a391699022e252351a
George Cooper <coopsgeorge@gmail.com>
2020-03-08 10:37:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,25 +18,19 @@ pigeon_data = {
 }
 
 def nyc_pigeon_organizer(data)
-  final = {}
+final_hash = {}
 
-  data.each do |first_level, all_other|
-    all_other.each do |category, array|
-      array.each do |name|
-        final[name] = {:color => [], :gender => [], :lives => []}
-      end
+data.each do |colour_gender_lives, pigeon_category|
+  pigeon_category.each do |pigeon_attribute, array|
+    array.each do |pigeon_name|
+    final_hash[pigeon_name] = {:color => [], :gender => [], :lives => []}
     end
   end
-  x = final.keys
-  data[:color].each do |bird_color, name|
-    name.each do |bird_name|
-      x.each do |item|
-        if bird_name === item
-          final[item][:color] << bird_color.to_s
-        end
-      end
-    end
-  end
+end
 
-  final
+
+
+
+
+final_hash
 end
